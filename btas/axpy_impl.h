000fe7ab3df2ae5a6f6041df320a44e98ca6bbdf
naokin <n.nakatani0608@gmail.com>
2013-11-19 11:51:27 +0900

    Fixed bug on axpy_impl, now it works (see test/test.C for usage)

--- btas/axpy_impl.h
+++ btas/axpy_impl.h
@@ -3,6 +3,43 @@
 
 namespace btas {
 
+/// Call BLAS depending on type of Tensor class
+template<bool _DoAxpy> struct axpy_impl { };
+
+/// Case that alpha is trivially multipliable to elements
+template<> struct axpy_impl<true>
+{
+   template<typename _T, class _Tensor>
+   axpy_impl(const _T& alpha, const _Tensor& x, _Tensor& y)
+   {
+      auto ix = x.begin();
+      auto iy = y.begin();
+      while(ix != x.end())
+      {
+         (*iy) += alpha * (*ix);
+         ++ix;
+         ++iy;
+      }
+   }
+};
+
+/// Case that alpha is multiplied recursively by AXPY
+template<> struct axpy_impl<false>
+{
+   template<typename _T, class _Tensor>
+   axpy_impl(const _T& alpha, const _Tensor& x, _Tensor& y)
+   {
+      auto ix = x.begin();
+      auto iy = y.begin();
+      while(ix != x.end())
+      {
+         axpy(alpha, *ix, *iy);
+         ++ix;
+         ++iy;
+      }
+   }
+};
+
 /// Generic implementation of BLAS-AXPY
 template<typename _T, class _Tensor>
 void axpy(const _T& alpha, const _Tensor& x, _Tensor& y)
@@ -23,43 +60,9 @@ void axpy(const _T& alpha, const _Tensor& x, _Tensor& y)
    }
 
    typedef typename std::iterator_traits<typename _Tensor::iterator>::value_type value_type;
-   axpy_impl<std::is_same<_T, value_type>::value> call(alpha, x, y);
-// FIXME: more general?
-// axpy_impl<std::is_convertible<_T, value_type>::value> call(alpha, x, y);
+// axpy_impl<std::is_same<_T, value_type>::value> call(alpha, x, y);
+   axpy_impl<std::is_convertible<_T, value_type>::value> call(alpha, x, y);
 }
-
-/// Call BLAS depending on type of Tensor class
-template<bool _DoAxpy> struct axpy_impl { };
-
-/// Case that alpha is trivially multipliable to elements
-template<> struct axpy_impl<true>
-{
-   template<typename _T, class _Tensor>
-   axpy_impl(const _T& alpha, const _Tensor& x, _Tensor& y)
-   {
-      auto ix = x.begin();
-      auto iy = y.begin();
-      while(ix != x.end())
-      {
-         (*iy) += alpha * (*ix);
-      }
-   }
-};
-
-/// Case that alpha is multiplied recursively by AXPY
-template<> struct axpy_impl<false>
-{
-   template<typename _T, class _Tensor>
-   axpy_impl(const _T& alpha, const _Tensor& x, _Tensor& y)
-   {
-      auto ix = x.begin();
-      auto iy = y.begin();
-      while(ix != x.end())
-      {
-         axpy(alpha, *ix, *iy);
-      }
-   }
-};
 
 } // namespace btas
 
--- test/test.C
+++ test/test.C
@@ -4,15 +4,16 @@ using namespace std;
 
 #include <tensor.h>
 #include <varray.h>
+#include <axpy_impl.h>
 using namespace btas;
 
 int main()
 {
    // test 1
-   Tensor<double> T(2,2,2);
-   fill(T.begin(), T.end(), 0.0);
+   Tensor<double> T(2,2,2); T.fill(0.0);
 
    T(1,0,1) = -0.5;
+
    T.at(1,1,0) = 0.5;
 
    cout << "printing T: size = " << T.size() << " objsize = " << sizeof(T) << endl;
@@ -21,8 +22,7 @@ int main()
    // test 2
    typedef Tensor<float, varray<float>, varray<int>> MyTensor;
    MyTensor::shape_type shape = { 4, 4 };
-   MyTensor Q(shape);
-   fill(Q.begin(), Q.end(), 1.0);
+   MyTensor Q(shape); Q.fill(2.0);
 
    MyTensor::shape_type index = { 1, 2 };
 
@@ -33,13 +33,18 @@ int main()
    cout << "printing Q: size = " << Q.size() << " objsize = " << sizeof(Q) << endl;
    for(double x : Q) cout << x << endl;
 
-// // test 3
-// Slice<MyTensor>::index_type lb = { 1, 1 };
-// Slice<MyTensor>::index_type ub = { 3, 3 };
-// Slice<MyTensor> S(Q, lb, ub);
+   // test 3
+   Tensor<double> S(2,2,2); S.fill(1.0);
+   axpy(0.5, T, S);
 
-// cout << "printing S: size = " << S.size() << endl;
-// for(double x : S) cout << x << endl;
+   cout << "printing S: size = " << S.size() << " objsize = " << sizeof(S) << endl;
+   for(double x : S) cout << x << endl;
+
+   Tensor<double> U;
+   axpy(0.5, S, U);
+
+   cout << "printing U: size = " << U.size() << " objsize = " << sizeof(U) << endl;
+   for(double x : U) cout << x << endl;
 
    return 0;
 }
