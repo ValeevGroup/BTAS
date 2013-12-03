ValeevGroup/BTAS

commit 4ddc2d890c7db800c7ca241dba3b45453f0a9b8e

Slight optimization of NDIterator::__increment() and __decrement().

btas/nditerator.h
@@ -353,15 +353,9 @@ private:
       }
 
       // reaching the last
-      if(i == 0)
-      {
-         ++index_[i];
-         current_ = __get__address();
-      }
-      else
-      {
-         current_ += offset;
-      }
+      if(i == 0) ++index_[i];
+
+      current_ += offset;
    }
 
    /// decrement
@@ -394,15 +388,9 @@ private:
       }
 
       // reaching the last
-      if(i == 0)
-      {
-         --index_[i];
-         current_ = __get__address();
-      }
-      else
-      {
-         current_ -= offset;
-      }
+      if(i == 0) --index_[i];
+
+      current_ -= offset;
    }
 
 };

commit 2dfadaeaae74f2e40c0146dadfc843664314a4f0

Undo bug introduced by 4ddc2d890c7db

btas/nditerator.h
@@ -366,9 +366,15 @@ private:
       }
 
       // reaching the last
-      if(i == 0) ++index_[i];
-
-      current_ += offset;
+      if(i == 0) 
+          {
+          ++index_[i];
+          current_ = __get__address();
+          }
+      else
+          {
+          current_ += offset;
+          }
    }
 
    /// decrement
@@ -401,9 +407,15 @@ private:
       }
 
       // reaching the last
-      if(i == 0) --index_[i];
-
-      current_ -= offset;
+      if(i == 0) 
+          {
+          --index_[i];
+          current_ = __get__address();
+          }
+      else
+          {
+          current_ -= offset;
+          }
    }
 
 };