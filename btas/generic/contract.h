be37a3f2ce0ec71b5332f81599822a5d555df04a
Eduard Valeyev <eduard@valeyev.net>
2014-01-03 12:40:06 -0500

    Updated contract.h; switched from postfix to prefix form for begin, end, extent, and rank.

--- btas/generic/contract.h
+++ btas/generic/contract.h
@@ -54,18 +54,18 @@ void contract(
 {
    // check index A
    auto __sort_indexA = _AnnotationA{aA};
-   std::sort(__sort_indexA.begin(), __sort_indexA.end());
-   assert(std::unique(__sort_indexA.begin(), __sort_indexA.end()) == __sort_indexA.end());
+   std::sort(std::begin(__sort_indexA), std::end(__sort_indexA));
+   assert(std::unique(std::begin(__sort_indexA), std::end(__sort_indexA)) == std::end(__sort_indexA));
 
    // check index B
    auto __sort_indexB = _AnnotationB{aB};
-   std::sort(__sort_indexB.begin(), __sort_indexB.end());
-   assert(std::unique(__sort_indexB.begin(), __sort_indexB.end()) == __sort_indexB.end());
+   std::sort(std::begin(__sort_indexB), std::end(__sort_indexB));
+   assert(std::unique(std::begin(__sort_indexB), std::end(__sort_indexB)) == std::end(__sort_indexB));
 
    // check index C
    auto __sort_indexC = _AnnotationC{aC};
-   std::sort(__sort_indexC.begin(), __sort_indexC.end());
-   assert(std::unique(__sort_indexC.begin(), __sort_indexC.end()) == __sort_indexC.end());
+   std::sort(std::begin(__sort_indexC), std::end(__sort_indexC));
+   assert(std::unique(std::begin(__sort_indexC), std::end(__sort_indexC)) == std::end(__sort_indexC));
 
    typedef btas::varray<size_t> Permutation;
 
@@ -86,9 +86,9 @@ void contract(
    size_type k = 0;
 
    // row index
-   for(auto itrA = aA.begin(); itrA != aA.end(); ++itrA)
+   for(auto itrA = std::begin(aA); itrA != std::end(aA); ++itrA)
    {
-      if(!std::binary_search(__sort_indexB.begin(), __sort_indexB.end(), *itrA))
+      if(!std::binary_search(std::begin(__sort_indexB), std::end(__sort_indexB), *itrA))
       {
          __permute_indexA[m] = *itrA;
          __permute_indexC[m] = *itrA;
@@ -96,9 +96,9 @@ void contract(
       }
    }
    // index to be contracted
-   for(auto itrA = aA.begin(); itrA != aA.end(); ++itrA)
+   for(auto itrA = std::begin(aA); itrA != std::end(aA); ++itrA)
    {
-      if( std::binary_search(__sort_indexB.begin(), __sort_indexB.end(), *itrA))
+      if( std::binary_search(std::begin(__sort_indexB), std::end(__sort_indexB), *itrA))
       {
          __permute_indexA[m+k] = *itrA;
          __permute_indexB[k]   = *itrA;
@@ -106,9 +106,9 @@ void contract(
       }
    }
    // column index
-   for(auto itrB = aB.begin(); itrB != aB.end(); ++itrB)
+   for(auto itrB = std::begin(aB); itrB != std::end(aB); ++itrB)
    {
-      if(!std::binary_search(__sort_indexA.begin(), __sort_indexA.end(), *itrB))
+      if(!std::binary_search(std::begin(__sort_indexA), std::end(__sort_indexA), *itrB))
       {
          __permute_indexB[k+n] = *itrB;
          __permute_indexC[m+n] = *itrB;
@@ -118,12 +118,12 @@ void contract(
 
    // check result index C
    Permutation __sort_permute_indexC(__permute_indexC);
-   std::sort(__sort_permute_indexC.begin(), __sort_permute_indexC.end());
-   assert(std::equal(__sort_permute_indexC.begin(), __sort_permute_indexC.end(), __sort_indexC.begin()));
+   std::sort(std::begin(__sort_permute_indexC), std::end(__sort_permute_indexC));
+   assert(std::equal(std::begin(__sort_permute_indexC), std::end(__sort_permute_indexC), std::begin(__sort_indexC)));
 
    // permute A
    std::shared_ptr<const _TensorA> __refA;
-   if(!std::equal(aA.begin(), aA.end(), __permute_indexA.begin()))
+   if(!std::equal(std::begin(aA), std::end(aA), std::begin(__permute_indexA)))
    {
       __refA = std::make_shared<const _TensorA>();
       permute(A, aA, const_cast<_TensorA&>(*__refA), __permute_indexA);
@@ -135,7 +135,7 @@ void contract(
 
    // permute B
    std::shared_ptr<const _TensorB> __refB;
-   if(!std::equal(aB.begin(), aB.end(), __permute_indexB.begin()))
+   if(!std::equal(std::begin(aB), std::end(aB), std::begin(__permute_indexB)))
    {
       __refB = std::make_shared<const _TensorB>();
       permute(B, aB, const_cast<_TensorB&>(*__refB), __permute_indexB);
@@ -152,13 +152,13 @@ void contract(
    {
       Permutation __zero_shape;
       resize(__zero_shape, m+n);
-      std::fill(__zero_shape.begin(), __zero_shape.end(), 0);
+      std::fill(std::begin(__zero_shape), std::end(__zero_shape), 0);
       C.resize(__zero_shape);
    }
 
    // permute C
    std::shared_ptr<_TensorC> __refC;
-   if(!std::equal(aC.begin(), aC.end(), __permute_indexC.begin()))
+   if(!std::equal(std::begin(aC), std::end(aC), std::begin(__permute_indexC)))
    {
       __refC = std::make_shared<_TensorC>();
       permute(C, aC, *__refC, __permute_indexC);
@@ -170,7 +170,7 @@ void contract(
    }
 
    // call BLAS functions
-   if     (A.rank() == k && B.rank() == k)
+   if     (rank(A) == k && rank(B) == k)
    {
       assert(false); // dot should be called instead
    }
@@ -179,11 +179,11 @@ void contract(
       scal(beta, *__refC);
       ger (alpha, *__refA, *__refB, *__refC);
    }
-   else if(A.rank() == k)
+   else if(rank(A) == k)
    {
       gemv(CblasTrans,   alpha, *__refB, *__refA, beta, *__refC);
    }
-   else if(B.rank() == k)
+   else if(rank(B) == k)
    {
       gemv(CblasNoTrans, alpha, *__refA, *__refB, beta, *__refC);
    }
