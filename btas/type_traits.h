cf11312eeaf0ae6ef56a9c2878acffa55d55237e
Eduard Valeyev <eduard@valeyev.net>
2014-01-03 12:32:29 -0500

    Moved basic checks from index_traits to type_traits

--- btas/type_traits.h
+++ btas/type_traits.h
@@ -16,6 +16,71 @@ namespace btas {
         >::type type;
   }; // common_signed_type
 
+  /// test T has begin() member
+  template<class T>
+  class has_begin {
+      /// true case
+      template<class U>
+      static auto __test(U* p) -> decltype(p->begin(), std::true_type());
+      /// false case
+      template<class >
+      static std::false_type __test(...);
+    public:
+      static constexpr const bool value = std::is_same<std::true_type,
+          decltype(__test<T>(0))>::value;
+  };
+
+  /// test T has end() member
+  template<class T>
+  class has_end {
+      /// true case
+      template<class U>
+      static auto __test(U* p) -> decltype(p->end(), std::true_type());
+      /// false case
+      template<class >
+      static std::false_type __test(...);
+    public:
+      static constexpr const bool value = std::is_same<std::true_type,
+          decltype(__test<T>(0))>::value;
+  };
+
+  /// test T has value_type
+  template<class T>
+  class has_value_type {
+      /// true case
+      template<class U>
+      static std::true_type __test(typename U::value_type*);
+      /// false case
+      template<class >
+      static std::false_type __test(...);
+    public:
+      static constexpr const bool value = std::is_same<std::true_type,
+          decltype(__test<T>(0))>::value;
+  };
+
+  /// test _C conforms to the standard Container concept; basic tests only
+  template<class _C>
+  class is_container {
+    public:
+      static constexpr const bool value = has_value_type<_C>::value
+          & has_begin<_C>::value & has_end<_C>::value;
+  };
+
+  /// test T has operator[] member
+  template<class T>
+  class has_squarebraket {
+      /// true case
+      template<class U>
+      static auto __test(
+          U* p, std::size_t i) -> decltype(p->operator[](i), std::true_type());
+      /// false case
+      template<class >
+      static std::false_type __test(...);
+    public:
+      static constexpr const bool value = std::is_same<std::true_type,
+          decltype(__test<T>(0,std::size_t(0)))>::value;
+  };
+
 } // namespace btas
 
 #endif // __BTAS_TYPE_TRAITS_H
