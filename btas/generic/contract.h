c55904de23c0651df8fe99b99f84dcce66c2345b
Eduard Valeyev <eduard@valeyev.net>
2014-01-03 12:38:03 -0500

    Updated “annotated” contract.

--- btas/generic/contract.h
+++ btas/generic/contract.h
@@ -35,55 +35,58 @@ namespace btas {
 template<
    typename _T,
    class _TensorA, class _TensorB, class _TensorC,
+   class _AnnotationA, class _AnnotationB, class _AnnotationC,
    class = typename std::enable_if<
-      is_tensor<_TensorA>::value &
-      is_tensor<_TensorB>::value &
-      is_tensor<_TensorC>::value &
-      std::is_same<typename _TensorA::value_type, typename _TensorB::value_type>::value &
-      std::is_same<typename _TensorA::value_type, typename _TensorC::value_type>::value
+      is_boxtensor<_TensorA>::value &
+      is_boxtensor<_TensorB>::value &
+      is_boxtensor<_TensorC>::value &
+      is_container<_AnnotationA>::value &
+      is_container<_AnnotationB>::value &
+      is_container<_AnnotationC>::value
    >::type
 >
 void contract(
-   const CBLAS_ORDER& order,
    const _T& alpha,
-   const _TensorA& A, const typename _TensorA::shape_type& indexA,
-   const _TensorB& B, const typename _TensorB::shape_type& indexB,
+   const _TensorA& A, const _AnnotationA& aA,
+   const _TensorB& B, const _AnnotationB& aB,
    const _T& beta,
-         _TensorC& C, const typename _TensorC::shape_type& indexC)
+         _TensorC& C, const _AnnotationC& aC)
 {
    // check index A
-   typename _TensorA::shape_type __sort_indexA(indexA);
+   auto __sort_indexA = _AnnotationA{aA};
    std::sort(__sort_indexA.begin(), __sort_indexA.end());
    assert(std::unique(__sort_indexA.begin(), __sort_indexA.end()) == __sort_indexA.end());
 
    // check index B
-   typename _TensorB::shape_type __sort_indexB(indexB);
+   auto __sort_indexB = _AnnotationB{aB};
    std::sort(__sort_indexB.begin(), __sort_indexB.end());
    assert(std::unique(__sort_indexB.begin(), __sort_indexB.end()) == __sort_indexB.end());
 
    // check index C
-   typename _TensorC::shape_type __sort_indexC(indexC);
+   auto __sort_indexC = _AnnotationC{aC};
    std::sort(__sort_indexC.begin(), __sort_indexC.end());
    assert(std::unique(__sort_indexC.begin(), __sort_indexC.end()) == __sort_indexC.end());
 
+   typedef btas::varray<size_t> Permutation;
+
    // permute index A
-   typename _TensorA::shape_type __permute_indexA;
-   resize(__permute_indexA, indexA.size());
+   Permutation __permute_indexA;
+   resize(__permute_indexA, aA.size());
 
    // permute index B
-   typename _TensorB::shape_type __permute_indexB;
-   resize(__permute_indexB, indexB.size());
+   Permutation __permute_indexB;
+   resize(__permute_indexB, aB.size());
 
    // permute index C
-   typename _TensorC::shape_type __permute_indexC;
-   resize(__permute_indexC, indexC.size());
+   Permutation __permute_indexC;
+   resize(__permute_indexC, aC.size());
 
    size_type m = 0;
    size_type n = 0;
    size_type k = 0;
 
    // row index
-   for(auto itrA = indexA.begin(); itrA != indexA.end(); ++itrA)
+   for(auto itrA = aA.begin(); itrA != aA.end(); ++itrA)
    {
       if(!std::binary_search(__sort_indexB.begin(), __sort_indexB.end(), *itrA))
       {
@@ -93,7 +96,7 @@ void contract(
       }
    }
    // index to be contracted
-   for(auto itrA = indexA.begin(); itrA != indexA.end(); ++itrA)
+   for(auto itrA = aA.begin(); itrA != aA.end(); ++itrA)
    {
       if( std::binary_search(__sort_indexB.begin(), __sort_indexB.end(), *itrA))
       {
@@ -103,7 +106,7 @@ void contract(
       }
    }
    // column index
-   for(auto itrB = indexB.begin(); itrB != indexB.end(); ++itrB)
+   for(auto itrB = aB.begin(); itrB != aB.end(); ++itrB)
    {
       if(!std::binary_search(__sort_indexA.begin(), __sort_indexA.end(), *itrB))
       {
@@ -114,16 +117,16 @@ void contract(
    }
 
    // check result index C
-   typename _TensorC::shape_type __sort_permute_indexC(__permute_indexC);
+   Permutation __sort_permute_indexC(__permute_indexC);
    std::sort(__sort_permute_indexC.begin(), __sort_permute_indexC.end());
    assert(std::equal(__sort_permute_indexC.begin(), __sort_permute_indexC.end(), __sort_indexC.begin()));
 
    // permute A
    std::shared_ptr<const _TensorA> __refA;
-   if(!std::equal(indexA.begin(), indexA.end(), __permute_indexA.begin()))
+   if(!std::equal(aA.begin(), aA.end(), __permute_indexA.begin()))
    {
       __refA = std::make_shared<const _TensorA>();
-      permute(A, indexA, const_cast<_TensorA&>(*__refA), __permute_indexA);
+      permute(A, aA, const_cast<_TensorA&>(*__refA), __permute_indexA);
    }
    else
    {
@@ -132,10 +135,10 @@ void contract(
 
    // permute B
    std::shared_ptr<const _TensorB> __refB;
-   if(!std::equal(indexB.begin(), indexB.end(), __permute_indexB.begin()))
+   if(!std::equal(aB.begin(), aB.end(), __permute_indexB.begin()))
    {
       __refB = std::make_shared<const _TensorB>();
-      permute(B, indexB, const_cast<_TensorB&>(*__refB), __permute_indexB);
+      permute(B, aB, const_cast<_TensorB&>(*__refB), __permute_indexB);
    }
    else
    {
@@ -147,7 +150,7 @@ void contract(
    // to set rank of C
    if(C.empty())
    {
-      typename _TensorC::shape_type __zero_shape;
+      Permutation __zero_shape;
       resize(__zero_shape, m+n);
       std::fill(__zero_shape.begin(), __zero_shape.end(), 0);
       C.resize(__zero_shape);
@@ -155,10 +158,10 @@ void contract(
 
    // permute C
    std::shared_ptr<_TensorC> __refC;
-   if(!std::equal(indexC.begin(), indexC.end(), __permute_indexC.begin()))
+   if(!std::equal(aC.begin(), aC.end(), __permute_indexC.begin()))
    {
       __refC = std::make_shared<_TensorC>();
-      permute(C, indexC, *__refC, __permute_indexC);
+      permute(C, aC, *__refC, __permute_indexC);
       __C_to_permute = true;
    }
    else
@@ -174,26 +177,53 @@ void contract(
    else if(k == 0)
    {
       scal(beta, *__refC);
-      ger (order, alpha, *__refA, *__refB, *__refC);
+      ger (alpha, *__refA, *__refB, *__refC);
    }
    else if(A.rank() == k)
    {
-      gemv(order, CblasTrans,   alpha, *__refB, *__refA, beta, *__refC);
+      gemv(CblasTrans,   alpha, *__refB, *__refA, beta, *__refC);
    }
    else if(B.rank() == k)
    {
-      gemv(order, CblasNoTrans, alpha, *__refA, *__refB, beta, *__refC);
+      gemv(CblasNoTrans, alpha, *__refA, *__refB, beta, *__refC);
    }
    else
    {
-      gemm(order, CblasNoTrans, CblasNoTrans, alpha, *__refA, *__refB, beta, *__refC);
+      gemm(CblasNoTrans, CblasNoTrans, alpha, *__refA, *__refB, beta, *__refC);
    }
 
    // permute back
    if(__C_to_permute)
    {
-      permute(*__refC, __permute_indexC, C, indexC);
+      permute(*__refC, __permute_indexC, C, aC);
    }
+}
+
+template<
+   typename _T,
+   class _TensorA, class _TensorB, class _TensorC,
+   typename _UA, typename _UB, typename _UC,
+   class = typename std::enable_if<
+      is_tensor<_TensorA>::value &
+      is_tensor<_TensorB>::value &
+      is_tensor<_TensorC>::value &
+      std::is_same<typename _TensorA::value_type, typename _TensorB::value_type>::value &
+      std::is_same<typename _TensorA::value_type, typename _TensorC::value_type>::value
+   >::type
+>
+void contract(
+   const _T& alpha,
+   const _TensorA& A, std::initializer_list<_UA> aA,
+   const _TensorB& B, std::initializer_list<_UB> aB,
+   const _T& beta,
+         _TensorC& C, std::initializer_list<_UC> aC)
+{
+    contract(alpha,
+             A, btas::varray<_UA>(aA),
+             B, btas::varray<_UB>(aB),
+             beta,
+             C, btas::varray<_UC>(aC)
+            );
 }
 
 } //namespace btas
