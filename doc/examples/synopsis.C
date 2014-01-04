bcef98e40f49cf0f67b4d88fd4b0de1948b59816
Eduard Valeyev <eduard@valeyev.net>
2014-01-04 17:02:29 -0500

    Moved btas synopsis from doxygen comments to compilable source, see the examples directory

--- doc/examples/synopsis.C
+++ doc/examples/synopsis.C
@@ -12,15 +12,14 @@ using namespace btas;
 
 int main(int argc, char **argv) {
 
+  //! [Synopsis]
   auto O = Range1{2};              // [0, 2)
   auto U = Range1{2, 5};           // [2, 5)
   auto P = merge(O, U);            // [0, 5)
   auto OOUU = Range{O, O, U, U};   // direct product range
   auto PPPP = Range{P, P, P, P};
 
-  // "dense" Tensor
   typedef Tensor<double> DTensor;
-  // make a few Tensors
   DTensor t2(OOUU);
   std::fill(t2.begin(), t2.end(), 1.0); // fill t2 with ones
   DTensor v(PPPP);
@@ -31,8 +30,20 @@ int main(int argc, char **argv) {
 
   // Tensor contraction
   auto t2v = DTensor{};
-  // contract(1.0, t2, {'i','j','a','b'}, v_uuuu, {'a','b','c','d'}, 1.0, t2v, {'i','j','c','d'}); // error, can't pass TensorViews yet
-  contract(1.0, t2, {'i','j','a','b'}, DTensor{v_uuuu}, {'a','b','c','d'}, 1.0, t2v, {'i','j','c','d'});
+  contract(1.0,
+           t2, {'i','j','a','b'}, DTensor{v_uuuu}, {'a','b','c','d'},  // v_uuuu is copied to temporary
+                                                                       // SOON: TensorViews will be usable interchangeably with Tensors
+           0.0, t2v, {'i','j','c','d'});
+
+  // The default Tensor is a "dense" box of values, with internally managed storage
+  // But Tensor is highly configurable
+  // For example, it can be used with externally-managed storage
+  {
+    auto mybuf = std::vector<double>{OOUU.area()};
+    typedef Tensor<double, Range, StorageRef<std::vector<double> >  > DTensor;
+    auto v_oouu = DTensor(OOUU, ref);
+  }
+  //! [Synopsis]
 
   return 0;
 }
