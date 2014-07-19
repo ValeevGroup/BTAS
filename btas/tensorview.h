a0aaaa57359cb295725caa853e1575ec9aaf2427
Eduard Valeyev <eduard@valeyev.net>
2014-07-19 16:22:32 -0700

    Implemented a TensorMap (and TensorConstMap) as a TensorView specification. Note the specialization of reference_wrapper for pointer wrapper infinite_sequence_adaptor.

--- btas/tensorview.h
+++ btas/tensorview.h
@@ -14,6 +14,7 @@
 #include <btas/tensorview_iterator.h>
 #include <btas/defaults.h>
 #include <btas/util/functional.h>
+#include <btas/util/sequence_adaptor.h>
 
 namespace btas {
 
@@ -85,7 +86,7 @@ namespace btas {
       template<class Policy = _Policy, class = typename std::enable_if<not Policy::runtimeconst>::type>
       explicit
       TensorView (range_type&& range,
-                  const storageref_type& storageref,
+                  storageref_type&& storageref,
                   bool can_write = not _Policy::runtimeconst ? not std::is_const<storage_type>::value : false) :
       range_(range), storageref_(storageref), can_write_(can_write)
       {
@@ -233,7 +234,7 @@ namespace btas {
       storage()
       {
         assert_writable();
-        return storageref_;
+        return storageref_.get();
       }
 
       /// test whether TensorView is empty
@@ -477,10 +478,10 @@ namespace btas {
                 typename Storage>
       friend TensorView<T, Range, const Storage> make_cview(const Range& range, const Storage& storage);
 
-      template <typename T,
-                typename Range>
-      friend TensorView<typename std::remove_const<T>::type, Range, typename std::conditional<std::is_const<T>::value,T* const,T*>::type>
-      make_map(T*& data, Range&& range);
+//      template <typename T,
+//                typename Range>
+//      friend TensorView<typename std::remove_const<T>::type, Range, typename std::conditional<std::is_const<T>::value,T* const,T*>::type>
+//      make_map(T*& data, Range&& range);
 
       template <class __T,
                       class __Range,
@@ -679,11 +680,11 @@ namespace btas {
   /// TensorMap views a sequence of values as a Tensor
   template <typename _T,
             class _Range = btas::DEFAULT::range>
-  using TensorMap = TensorView<_T, _Range, _T*>;
+  using TensorMap = TensorView<_T, _Range, btas::infinite_sequence_adaptor<_T*>>;
   /// TensorConstMap const-views a sequence of values as a Tensor
   template <typename _T,
             class _Range = btas::DEFAULT::range>
-  using TensorConstMap = TensorView<_T, _Range, const _T* const>;
+  using TensorConstMap = TensorView<_T, _Range, const btas::infinite_sequence_adaptor<const _T*>>;
 
   /// Helper function that constructs TensorMap.
   /// \tparam T the element type returned by the view
@@ -693,14 +694,26 @@ namespace btas {
   /// \attention use make_cmap if you must force a const view; this will provide const view, however, if \c storage is a const reference.
   template <typename T,
             typename Range>
-  TensorView<typename std::remove_const<T>::type, Range, typename std::conditional<std::is_const<T>::value,T* const,T*>::type>
-  make_map(T*& data, Range&& range)
+  TensorMap<T, Range>
+  make_map(T* data, Range&& range)
   {
-    typedef typename std::remove_const<T>::type value_type;
-    typedef TensorView<value_type, Range, typename std::conditional<std::is_const<T>::value,T* const,T*>::type>
-        result_type;
-    return result_type(std::move(range),
-                       std::ref(data));
+    return TensorMap<T, Range>(std::move(range),
+                               std::ref(btas::infinite_sequence_adaptor<T*>(data)));
+  }
+
+  /// Helper function that constructs TensorConstMap.
+  /// \tparam T the element type returned by the view
+  /// \tparam Range the range type
+  /// \param range the range object defining the view
+  /// \return TensorView into \c storage using \c range
+  /// \attention use make_cmap if you must force a const view; this will provide const view, however, if \c storage is a const reference.
+  template <typename T,
+            typename Range>
+  TensorConstMap<T, Range>
+  make_map(const T* data, Range&& range)
+  {
+    return TensorConstMap<T, Range>(std::move(range),
+                                    std::cref(btas::infinite_sequence_adaptor<const T*>(data)));
   }
 
   /// Helper function that constructs TensorConstMap.
@@ -711,12 +724,12 @@ namespace btas {
   template <typename T,
             typename Range>
   TensorConstMap<typename std::remove_const<T>::type, Range>
-  make_cmap(T*& data, Range&& range)
+  make_cmap(T* data, Range&& range)
   {
     typedef typename std::remove_const<T>::type value_type;
-    TensorConstMap<value_type, Range> result(std::move(range),
-                                             std::cref(const_cast<const T*&>(data)));
-    return result;
+    typedef TensorConstMap<value_type, Range> result_type;
+    return result_type(std::move(range),
+                       std::cref(btas::infinite_sequence_adaptor<const T*>(const_cast<const T*>(data))));
   }
 
 } // namespace btas
--- test/test.C
+++ test/test.C
@@ -425,15 +425,15 @@ int main()
       for(auto i=0; i!=24; ++i) ptr0[i] = i;
 
       auto map0   = make_map(ptr0, Range{2, 3, 4});   // writable map
-      auto map0c  = make_map(const_cast<const double*&>(ptr0), Range{2, 3, 4});   // const map
+      auto map0c  = make_map(const_cast<const double*>(ptr0), Range{2, 3, 4});   // const map
       auto map0c2 = make_cmap(ptr0, Range{2, 3, 4}); // const map
 
       map0(1, 2, 3) = -1.0;
-      //map0c(1, 2, 3) = -1.0;
-      //map0c2(1, 2, 3) = -1.0;
+      //map0c(1, 2, 3) = -1.0;  // error: read-only map
+      //map0c2(1, 2, 3) = -1.0; // error: read-only map
 
-      std::cout << map0c << std::endl;
-      for (const auto& i: map0c) { std::cout << i << std::endl; }
+      std::cout << map0 << std::endl;
+      for (const auto& i: map0) { std::cout << i << std::endl; }
 
       delete[] ptr0;
   }
