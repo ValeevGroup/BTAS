f197849cfd514f36712724730eb25ed88abe62f2
Eduard Valeyev <eduard@valeyev.net>
2015-01-12 10:29:02 -0500

    minor doc fix

--- btas/serialization.h
+++ btas/serialization.h
@@ -6,6 +6,8 @@
 #include <boost/serialization/array.hpp>
 
 namespace boost { namespace serialization {
+  // this is needed to serialize  efficiently corner cases, like std::vector<std::array<std::complex<T>>>.
+  // since bitwise serialization is not portable anyway, this is OK in the context of btas
   template <typename T, size_t N>
   struct is_bitwise_serializable<std::array<T,N> > : is_bitwise_serializable<T> { };
 }}
