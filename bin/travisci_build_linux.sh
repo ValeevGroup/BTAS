06ebb2c865811cb68f3c231ccd5c5bccc42e76f7
Eduard Valeyev <eduard@valeyev.net>
2015-07-11 18:29:34 -0400

    [Travis-CI] Add USE_LAPACK cmake var and try testing with and without LAPACK

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -7,8 +7,16 @@ if [ "$CXX" = "g++" ]; then
     export CXX=/usr/bin/g++-$GCC_VERSION
 fi
 
-mkdir build
-cd build
+mkdir build_nolapack
+cd build_nolapack
 cmake .. -DBTAS_ASSERT_THROWS=ON
 make VERBOSE=1
 make check VERBOSE=1
+cd ..
+
+mkdir build_lapack
+cd build_lapack
+cmake .. -DBTAS_ASSERT_THROWS=ON -DUSE_LAPACK=ON
+make VERBOSE=1
+make check VERBOSE=1
+cd ..
