79bd6198f1e5153a4ef68c7a929172dc1377eaf8
Eduard Valeyev <eduard@valeyev.net>
2015-07-12 08:50:32 -0400

    Travis-CI: install libblas-dev

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -5,4 +5,4 @@ set -e
 sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update
-sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libboost1.55-dev libboost-serialization1.55-dev cmake
+sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libboost1.55-dev libboost-serialization1.55-dev cmake libblas-dev
