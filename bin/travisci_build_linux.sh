179e52032d1320145b5fdf970d86fe2397dfbd93
Eduard Valeyev <eduard@valeyev.net>
2015-08-03 18:21:32 -0400

    travis-ci: debugging FindCBLAS ...

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -10,13 +10,27 @@ fi
 ls -l /usr/lib
 ls -l /usr/lib/libblas
 
+##########   test with cblas   ##########
 mkdir build_cblas
 cd build_cblas
+
+cat <<EOF >test.cc
+#include<cblas.h>
+
+int main() {
+  double x = cblas_ddot(32, (double*)nullptr, 1, (double*)nullptr, 1);
+  return 0;
+}
+EOF
+
+$CXX -std=c++11 test.cc -lcblas -o test
+
 cmake .. -DBTAS_ASSERT_THROWS=ON -DUSE_CBLAS=ON
 make VERBOSE=1
 make check VERBOSE=1
 cd ..
 
+########## test without cblas ##########
 mkdir build
 cd build
 cmake .. -DBTAS_ASSERT_THROWS=ON
