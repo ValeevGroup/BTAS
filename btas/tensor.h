20f6607a6d393c3ef1dcc6ad1bed1b5a4e5464aa
Eduard Valeyev <eduard@valeyev.net>
2018-01-22 11:49:50 -0500

    Revert "Revert "improved Tensor's move assignment operator""
    This reverts commit 8eed03c5634a1b18f20cd067dce02cea2f9c1112.

--- btas/tensor.h
+++ btas/tensor.h
@@ -255,8 +255,9 @@ namespace btas {
       Tensor&
       operator= (Tensor&& x)
       {
-        std::swap(range_, x.range_);
-        std::swap(storage_, x.storage_);
+        using std::swap;
+        swap(range_, x.range_);
+        swap(storage_, x.storage_);
         return *this;
       }
 
