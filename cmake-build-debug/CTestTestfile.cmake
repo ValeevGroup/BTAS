eb33cfed20334795d715b1f035fdadacec48b102
Karl Pierce <karlpierce@Karls-MacBook-Pro.local>
2018-01-23 10:45:49 -0500

    Orthogonalize random metric, non-sequential truncation in randomize.h

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/karlpierce/software/BTAS
-# Build directory: /Users/karlpierce/software/BTAS/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doc")
-subdirs("unittest")
--- cmake-build-debug/unittest/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/karlpierce/software/BTAS/unittest
-# Build directory: /Users/karlpierce/software/BTAS/cmake-build-debug/unittest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(btas_test "/Users/karlpierce/software/BTAS/cmake-build-debug/unittest/btas_test" "-s")
-set_tests_properties(btas_test PROPERTIES  ENVIRONMENT "LD_LIBRARY_PATH=/opt/intel/compilers_and_libraries_2018.1.126/mac/tbb/lib:/opt/intel/compilers_and_libraries_2018.1.126/mac/compiler/lib:/opt/intel/compilers_and_libraries_2018.1.126/mac/mkl/lib" WORKING_DIRECTORY "/Users/karlpierce/software/BTAS/unittest")
