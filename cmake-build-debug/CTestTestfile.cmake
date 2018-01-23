c621c07ba23ecada3c89e0d399c229f18746b003
Karl Pierce <karlpierce@Karls-MacBook-Pro.local>
2018-01-23 10:45:11 -0500

    Orthogonalize random metric, non-sequential truncation in randomize.h

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/karlpierce/software/BTAS
+# Build directory: /Users/karlpierce/software/BTAS/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("unittest")
