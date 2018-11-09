347ccb49f6985548748bcf5be4c046cc03256a2c
kmp5VT <karlpierce@Karls-MacBook-Pro.local>
2018-11-09 09:11:07 -0500

    Update unittests with new converge test, function calls and use fast pseudo-inverse.

--- unittest/tensor_cp_test.cc
+++ unittest/tensor_cp_test.cc
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include "btas/btas.h"
+#include "btas/generic/converge_class.h"
 
 using namespace btas;
 typedef btas::Tensor<double> tensor;
@@ -54,18 +55,19 @@ TEST_CASE("CP_ALS"){
   for(auto& i: results){
     res >> i;
   }
+  NORM_CHECK<tensor> conv(1e-2);
 
   SECTION("MODE = 3, Finite rank"){
   	{
       CP_ALS<tensor> A1(D3);
-      double diff = A1.compute_rank(5,false,true, 1, 1e3, 1e-2);
+      double diff = A1.compute_rank(5, conv, 1, false, 0, false, 1e4, false, true);
       CHECK((diff - results(0,0)) <= epsilon);
     }
   }
   SECTION("MODE = 3, Finite error"){
   	{
       CP_ALS<tensor> A1(D3);
-      double diff = A1.compute_error(1e-2, true, 1, 1e3, 1e-2);
+      double diff = A1.compute_error(conv, 1e-2, 1, 1e3);
       CHECK((diff - results(1,0)) <= epsilon);
     }
   }
@@ -73,14 +75,14 @@ TEST_CASE("CP_ALS"){
   SECTION("MODE = 3, Tucker + CP"){
   	{
       CP_ALS<tensor> A1(D3);
-      double diff = A1.compress_compute_tucker(.1, false, 1e-2, 5, true, true);
+      double diff = A1.compress_compute_tucker(0.1, conv, false, 1e-2, 5, true, true);
       CHECK((diff - results(2,0)) <= epsilon);
     }
   }
   SECTION("MODE = 3, Random + CP"){
   	{
       CP_ALS<tensor> A1(D3);
-      double diff = A1.compress_compute_rand(2, 0, 2, false, 1e-2, 5, true, true );
+      double diff = A1.compress_compute_rand(2, conv, 0, 2, false, 1e-2, 5, true, true);
       CHECK((diff - results(3,0)) <= epsilon);
     }
   }
@@ -89,14 +91,14 @@ TEST_CASE("CP_ALS"){
   SECTION("MODE = 4, Finite rank"){
   	{
       CP_ALS<tensor> A1(D4);
-      double diff = A1.compute_rank(5,false,true, 1, 1e3, 1e-2);
+      double diff = A1.compute_rank(5, conv, 1, false, 0, false, 1e3, false, true, false);
       CHECK((diff - results(4,0)) <= epsilon);
     }
   }
   SECTION("MODE = 4, Finite error"){
   	{
       CP_ALS<tensor> A1(D4);
-      double diff = A1.compute_error(1e-2, true, 1, 1e3, 1e-2);
+      double diff = A1.compute_error(conv, 1e-2, 1, 1e3);
       CHECK((diff - results(5,0)) <= epsilon);
     }
   }
@@ -104,14 +106,15 @@ TEST_CASE("CP_ALS"){
   SECTION("MODE = 4, Tucker + CP"){
   	{
       CP_ALS<tensor> A1(D4);
-      double diff = A1.compress_compute_tucker(.1, false, 1e-2, 5, true, true);
+      double diff = A1.compress_compute_tucker(0.1, conv, false, 1e-2, 5);
       CHECK((diff - results(6,0)) <= epsilon);
     }
   }
   SECTION("MODE = 4, Random + CP"){
   	{
       CP_ALS<tensor> A1(D4);
-      double diff = A1.compress_compute_rand(3, 0, 2, true, 1e-2 );      
+      double diff = A1.compress_compute_rand(3, conv, 0, 2, true, 1e-2);
+      std::cout << std::setprecision(16) << diff << std::endl;
       CHECK((diff - results(7,0)) <= epsilon);
     }
   }
@@ -120,14 +123,14 @@ TEST_CASE("CP_ALS"){
   SECTION("MODE = 5, Finite rank"){
   	{
       CP_ALS<tensor> A1(D5);
-      double diff = A1.compute_rank(5,false,true, 1, 1e3, 1e-2);
+      double diff = A1.compute_rank(5, conv, 1, false, 0, false, 1e4, true, true);
       CHECK((diff - results(8,0)) <= epsilon);
     }
   }
   SECTION("MODE = 5, Finite error"){
   	{
       CP_ALS<tensor> A1(D5);
-      double diff = A1.compute_error(1e-2, true, 1, 1e3, 1e-2);
+      double diff = A1.compute_error(conv, 0.8, 1, 50);
       CHECK((diff - results(9,0)) <= epsilon);
     }
   }
@@ -135,14 +138,14 @@ TEST_CASE("CP_ALS"){
   SECTION("MODE = 5, Tucker + CP"){
   	{
       CP_ALS<tensor> A1(D5);
-      double diff = A1.compress_compute_tucker(.1, false, 1e-2, 5, true, true);
+      double diff = A1. compress_compute_tucker(0.1, conv, false, 1e-2, 5, true, true);
       CHECK((diff - results(10,0)) <= epsilon);
     }
   }
   SECTION("MODE = 5, Random + CP"){
   	{
       CP_ALS<tensor> A1(D5);
-      double diff = A1. compress_compute_rand(1,0);
+      double diff = A1.compress_compute_rand(1, conv, 0, 2, false, 1e-2, 5, true, true);
       CHECK((diff - results(11,0)) <= epsilon);
     }
   }
