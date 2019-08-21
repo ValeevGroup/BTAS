f412ae1f81f8a9d2f432086b75852dfe53a55249
Eduard Valeyev <eduard@valeyev.net>
2019-08-21 08:41:25 -0400

    [travis] fixed source location in build script

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -17,7 +17,7 @@ cd ${BUILD_PREFIX}
 ##########   test with cblas   ##########
 mkdir build_cblas
 cd build_cblas
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DUSE_CBLAS=ON -DBoost_FOUND=ON -DBoost_INCLUDE_DIRS=/usr/include/boost -DBoost_LIBRARIES=/usr/lib/x86_64-linux-gnu/libboost_serialization.a -DSKIP_BOOST_SEARCH=ON
+cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DUSE_CBLAS=ON -DBoost_FOUND=ON -DBoost_INCLUDE_DIRS=/usr/include/boost -DBoost_LIBRARIES=/usr/lib/x86_64-linux-gnu/libboost_serialization.a -DSKIP_BOOST_SEARCH=ON
 make VERBOSE=1
 make check VERBOSE=1
 cd ..
@@ -25,7 +25,7 @@ cd ..
 ########## test without cblas ##########
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DBoost_FOUND=ON -DBoost_INCLUDE_DIRS=/usr/include/boost -DBoost_LIBRARIES=/usr/lib/x86_64-linux-gnu/libboost_serialization.a -DSKIP_BOOST_SEARCH=ON
+cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DBoost_FOUND=ON -DBoost_INCLUDE_DIRS=/usr/include/boost -DBoost_LIBRARIES=/usr/lib/x86_64-linux-gnu/libboost_serialization.a -DSKIP_BOOST_SEARCH=ON
 make VERBOSE=1
 make check VERBOSE=1
 cd ..
