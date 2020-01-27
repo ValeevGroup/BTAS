f3b3b7fa31f8cafd160923eab8d3c4100f16d92d
kmp5VT <kmp5@vt.edu>
2020-01-27 15:35:28 -0500

    Add description

--- btas/generic/default_random_seed.h
+++ btas/generic/default_random_seed.h
@@ -5,6 +5,7 @@
 #ifndef BTAS_GENERIC_DEFAULT_RANDOM_SEED_H
 #define BTAS_GENERIC_DEFAULT_RANDOM_SEED_H
 namespace btas{
+  // A seed for the random number generator.
   static inline unsigned int& random_seed_accessor(){
     static unsigned int value = 3;
     return value;
