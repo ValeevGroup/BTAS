e190a2770eea0ad77d0a2fdf0a2d4a69052c8db0
kmp5VT <kmp5@vt.edu>
2020-01-27 14:41:25 -0500

    Reminder to write documentation for pseudoinverse function

--- btas/generic/cp.h
+++ btas/generic/cp.h
@@ -613,8 +613,9 @@ namespace btas{
     /// second tryfast pseudo-inverse algorithm described in
     /// https://arxiv.org/pdf/0804.4809.pdf
     /// If all else fails use SVD
-    Tensor psuedoinverse_helper(int mode_of_A, bool & fast_pI,
-                                bool & cholesky, Tensor B = Tensor(),
+    // TODO write docs here
+    void psuedoinverse_helper(int mode_of_A, bool & fast_pI,
+                                bool & cholesky, Tensor & B = Tensor(),
                                 double lambda = 0.0){
       if(cholesky && B.empty()){
         BTAS_EXCEPTION("To compute the Cholesky, one must provide a LHS tensor (Ax = B)");
