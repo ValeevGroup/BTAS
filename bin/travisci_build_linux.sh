45d8e4237207892d5faf9d7d4d28203bed9b29ba
Eduard Valeyev <eduard@valeyev.net>
2020-01-28 12:09:46 -0500

    added exportable BTAS target (in BTAS:: namespace)

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -17,7 +17,7 @@ cd ${BUILD_PREFIX}
 ##########   test with blas+lapack   ##########
 mkdir build_cblas
 cd build_cblas
-cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DBoost_FOUND=ON -DBoost_INCLUDE_DIRS=/usr/include/boost -DBoost_LIBRARIES=/usr/lib/x86_64-linux-gnu/libboost_serialization.a -DSKIP_BOOST_SEARCH=ON
+cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON
 make VERBOSE=1
 make check VERBOSE=1
 cd ..
@@ -25,7 +25,7 @@ cd ..
 ########## test without blas+lapack   ##########
 mkdir build
 cd build
-cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DBTAS_USE_CBLAS_LAPACKE=OFF  -DBoost_FOUND=ON -DBoost_INCLUDE_DIRS=/usr/include/boost -DBoost_LIBRARIES=/usr/lib/x86_64-linux-gnu/libboost_serialization.a -DSKIP_BOOST_SEARCH=ON
+cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DBTAS_USE_CBLAS_LAPACKE=OFF
 make VERBOSE=1
 make check VERBOSE=1
 cd ..
