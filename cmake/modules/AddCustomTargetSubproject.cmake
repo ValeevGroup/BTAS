7aec3e3d1bbf0ce3b7bbb37fcb9b876e001eed40
Eduard Valeyev <eduard@valeyev.net>
2020-02-07 14:02:28 -0500

    added AddCustomTargetSubproject.cmake to automate creation of custom targets that detects and manages target name clashes

---
+++ cmake/modules/AddCustomTargetSubproject.cmake
@@ -0,0 +1,39 @@
+#
+# add_custom_target_subproject(proj X ...) defines custom target X-proj and
+# - if target X already exists, makes it depend on X-proj
+# - else creates target X depending on X-proj
+#
+# use case: if custom target names (e.g. "check", "doc", etc.) clash
+#           with other project's target when used as a subproject
+#
+# example: add_custom_target_subproject(myproject check USES_TERMINAL COMMAND ${CMAKE_CTEST_COMMAND} -V)
+#
+
+macro(add_custom_target_subproject _subproj _name)
+
+  set(extra_args "${ARGN}")
+  add_custom_target(${_name}-${_subproj} ${extra_args})
+
+  # does the newly-created target compiled by default?
+  list(FIND extra_args ALL target_built_by_default)
+
+  if (TARGET ${_name})
+    # is existing target ${_name} also compiled by default?
+    # warn if not, but this project's target is since that
+    # may indicate inconsistent creation of generic targets
+    get_target_property(supertarget_build_by_default ${_name} EXCLUDE_FROM_ALL)
+    if (${target_built_by_default} AND NOT ${supertarget_build_by_default})
+      message(WARNING "Created target ${_name}-${_subproj} is built by default but "super"-target ${_name} is not; perhaps it should be?")
+    endif()
+    add_dependencies(${_name} ${_name}-${_subproj})
+  else (TARGET ${_name})
+    # use ALL if given
+    if (target_built_by_default)
+      add_custom_target(${_name} ALL DEPENDS ${_name}-${_subproj})
+    else (target_built_by_default)
+      add_custom_target(${_name} DEPENDS ${_name}-${_subproj})
+    endif(target_built_by_default)
+  endif (TARGET ${_name})
+
+endmacro()
+
