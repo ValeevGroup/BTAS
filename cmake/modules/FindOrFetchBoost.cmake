7fc2bb6d1beeb949dd2f0e5113857df76c9d64ff
Eduard Valeyev <eduard@valeyev.net>
2020-12-10 12:41:32 -0500

    if Boost is missing will use https://github.com/Orphis/boost-cmake to build it. WARNING: results in unusable BTAS in install tree, to be fixed by https://github.com/Orphis/boost-cmake/pull/45

---
+++ cmake/modules/FindOrFetchBoost.cmake
@@ -0,0 +1,30 @@
+if (NOT TARGET Boost::boost)
+  include (FetchContent)
+
+  FetchContent_Declare(
+          CMAKEBOOST
+          GIT_REPOSITORY      https://github.com/Orphis/boost-cmake
+  )
+  FetchContent_MakeAvailable(CMAKEBOOST)
+  FetchContent_GetProperties(CMAKEBOOST
+          SOURCE_DIR CMAKEBOOST_SOURCE_DIR
+          BINARY_DIR CMAKEBOOST_BINARY_DIR
+          )
+
+  # current boost-cmake/master does not install boost correctly, so warn that installed BTAS will not be usable
+  # boost-cmake/install_rules https://github.com/Orphis/boost-cmake/pull/45 is supposed to fix it but is inactive
+  message(WARNING "Building Boost from source makes BTAS unusable from the install location! Install Boost using package manage or manually and reconfigure/reinstall BTAS to fix this")
+  install(TARGETS Boost_serialization EXPORT btas COMPONENT boost-libs)
+  export(EXPORT btas
+      FILE "${PROJECT_BINARY_DIR}/boost-targets.cmake")
+  install(EXPORT btas
+      FILE "boost-targets.cmake"
+      DESTINATION "${BTAS_INSTALL_CMAKEDIR}"
+      COMPONENT boost-libs)
+
+endif(NOT TARGET Boost::boost)
+
+# postcond check
+if (NOT TARGET Boost::boost)
+  message(FATAL_ERROR "FindOrFetchBOOST could not make Boost::boost target available")
+endif(NOT TARGET Boost::boost)
