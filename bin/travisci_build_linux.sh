eb78c935ba631f66c8f24c809fbec61c1b244efa
Eduard Valeyev <eduard@valeyev.net>
2020-12-11 16:45:47 -0500

    [travis] test without BLAS/LAPACK + cleanup

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -23,43 +23,18 @@ fi
 
 cd ${BUILD_PREFIX}
 
-##########   test with blas+lapack   ##########
-#mkdir build_cblas
-#cd build_cblas
-## control whether to use ILP64 or not by the parity of
-## GCC_VERSION + CLANG_VERSION
-#gccv=$GCC_VERSION
-#clangv=$([ "X$CLANG_VERSION" = "X" ] && echo "0" || echo "$CLANG_VERSION")
-#ilp64v=$(($gccv+$clangv))
-#export PREFER_ILP64=$((ilp64v % 2))
-## cannot link against static blas libs reliably so use shared ... other codes may still need to be able to configure BTAS for static linking
-#cmake ${TRAVIS_BUILD_DIR} -DBLA_STATIC=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_ASSERT_THROWS=ON -DBTAS_BUILD_UNITTEST=ON -DMKL_PREFER_ILP64=${PREFER_ILP64}
-#make VERBOSE=1
-#make check VERBOSE=1
-#cd ..
-
-
-########### build blaspp + lapackpp    ##########
-#export icl_install_prefix=$PWD/icl_install
-#git clone https://dbwy@bitbucket.org/icl/blaspp.git
-#git clone https://dbwy@bitbucket.org/icl/lapackpp.git
-#
-#mkdir -p blaspp/build && cd blaspp/build
-#cmake .. -DCMAKE_INSTALL_PREFIX=${icl_install} -Dbuild_tests=OFF
-#make -j2 install
-#
-#
-#mkdir -p lapackpp/build && cd lapackpp/build
-#cmake .. -DCMAKE_INSTALL_PREFIX=${icl_install} -DBUILD_LAPACKPP_TESTS=OFF
-#make -j2 install
-#cd ../..
-
-
-########## test without blas+lapack   ##########
-mkdir build
-cd build
+########## test with blas+lapack   ##########
+mkdir build_blaslapack
+cd build_blaslapack
 cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_PREFIX_PATH=${icl_install_prefix} -DBTAS_ASSERT_THROWS=ON  -DBTAS_BUILD_UNITTEST=ON
 make VERBOSE=1
 make check VERBOSE=1
 cd ..
 
+########## test without blas+lapack   ##########
+mkdir build_noblaslapack
+cd build_noblaslapack
+cmake ${TRAVIS_BUILD_DIR} -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBTAS_USE_BLAS_LAPACK=OFF -DCMAKE_PREFIX_PATH=${icl_install_prefix} -DBTAS_ASSERT_THROWS=ON  -DBTAS_BUILD_UNITTEST=ON
+make VERBOSE=1
+make check VERBOSE=1
+cd ..
