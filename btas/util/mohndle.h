0b2e92836253152299b3b4d7b37616c9c1e5072a
Eduard Valeyev <eduard@valeyev.net>
2021-06-23 17:25:38 -0500

    misc fixups, mohndle seems to work

--- btas/util/mohndle.h
+++ btas/util/mohndle.h
@@ -12,6 +12,8 @@
 #ifdef BTAS_HAS_BOOST_SERIALIZATION
 # include <boost/serialization/unique_ptr.hpp>
 # include <boost/serialization/shared_ptr.hpp>
+# include <boost/archive/binary_iarchive.hpp>
+# include <boost/archive/binary_oarchive.hpp>
 #endif
 
 #include <variant>
@@ -34,12 +36,13 @@ namespace btas {
   /// @tparam DefaultHandle the handle type to use when default constructing, or constructing storage object directly from a pack
   /// Enacpsulates a value, a reference, or a pointer to (bare, unique, or shared) to a contiguous storage
   template <typename Storage, Handle DefaultHandle = Handle::invalid, typename = std::enable_if_t<is_storage<Storage>::value>>
-  struct mohndle : std::variant<std::monostate, Storage, std::unique_ptr<Storage>, std::shared_ptr<Storage>,
-                                std::reference_wrapper<Storage>, Storage*> {
+  class mohndle : std::variant<std::monostate, Storage, std::unique_ptr<Storage>, std::shared_ptr<Storage>,
+                               std::reference_wrapper<Storage>, Storage*> {
+   public:
     using base_type = std::variant<std::monostate, Storage, std::unique_ptr<Storage>, std::shared_ptr<Storage>,
                                    std::reference_wrapper<Storage>, Storage*>;
 
-    //using base_type::base_type;
+    // using base_type::base_type;
 
     typedef typename storage_traits<Storage>::value_type value_type;
     typedef typename storage_traits<Storage>::pointer pointer;
@@ -52,23 +55,35 @@ namespace btas {
     typedef typename storage_traits<Storage>::iterator iterator;
     typedef typename storage_traits<Storage>::const_iterator const_iterator;
 
+    mohndle() = default;
+
     /// constructs mohndle from a handle
 
     /// @param handle a handle object
-    template <typename Handle, typename = std::enable_if_t<std::is_constructible_v<base_type,Handle&&>>>
+    template <typename Handle, typename = std::enable_if_t<std::is_constructible_v<base_type, Handle&&>>>
     explicit mohndle(Handle&& handle) : base_type(std::forward<Handle>(handle)) {}
 
-    mohndle(const mohndle& other) : base_type(std::visit(
-          [](auto&& v) -> base_type {
-            using v_t = std::remove_reference_t<decltype(v)>;
-            if constexpr (std::is_same_v<v_t, Storage> || std::is_same_v<v_t, Storage const> || std::is_same_v<v_t, std::reference_wrapper<Storage>> || std::is_same_v<v_t, std::reference_wrapper<Storage> const> || std::is_same_v<v_t, Storage*> || std::is_same_v<v_t, Storage* const> || std::is_same_v<v_t, std::shared_ptr<Storage>> || std::is_same_v<v_t, std::shared_ptr<Storage> const>) {
-              return v;
-            } else if constexpr (std::is_same_v<v_t, std::unique_ptr<Storage>> || std::is_same_v<v_t, std::unique_ptr<Storage> const>) {
-              return std::make_unique<Storage>(*(v.get()));
-            } else
-              abort();
-          },
-          other.base())) {}
+    mohndle(const mohndle& other)
+        : base_type(std::visit(
+              [](auto&& v) -> base_type {
+                using v_t = std::remove_reference_t<decltype(v)>;
+                if constexpr (std::is_same_v<v_t, Storage> || std::is_same_v<v_t, Storage const> ||
+                              std::is_same_v<v_t, std::reference_wrapper<Storage>> ||
+                              std::is_same_v<v_t, std::reference_wrapper<Storage> const> ||
+                              std::is_same_v<v_t, Storage*> || std::is_same_v<v_t, Storage* const> ||
+                              std::is_same_v<v_t, std::shared_ptr<Storage>> ||
+                              std::is_same_v<v_t, std::shared_ptr<Storage> const>) {
+                  return v;
+                } else if constexpr (std::is_same_v<v_t, std::unique_ptr<Storage>> ||
+                                     std::is_same_v<v_t, std::unique_ptr<Storage> const>) {
+                  return std::make_unique<Storage>(*(v.get()));
+                } else if constexpr (std::is_same_v<v_t, std::monostate> ||
+                                     std::is_same_v<v_t, std::monostate const>) {
+                  return {};
+                } else
+                  abort();
+              },
+              other.base())) {}
 
     mohndle(mohndle&&) = default;
 
@@ -81,8 +96,8 @@ namespace btas {
     ~mohndle() = default;
 
     /// constructs a mohndle of type given by DefaultHandle directly from zero or more arguments
-    template <typename ... Args, typename = std::enable_if_t<std::is_constructible_v<Storage, Args&&...>>>
-    explicit mohndle(Args&& ... args) {
+    template <typename... Args, typename = std::enable_if_t<std::is_constructible_v<Storage, Args&&...>>>
+    explicit mohndle(Args&&... args) {
       if constexpr (DefaultHandle == Handle::value)
         this->base().template emplace<Storage>(std::forward<Args>(args)...);
       else if constexpr (DefaultHandle == Handle::ptr)
@@ -103,34 +118,64 @@ namespace btas {
     }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_begin_v<S> && !std::is_const_v<S>, iterator> begin() { using std::begin; return begin(*(this->get())); }
+    std::enable_if_t<has_nonmember_begin_v<S> && !std::is_const_v<S>, iterator> begin() {
+      using std::begin;
+      return begin(*(this->get()));
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_end_v<S> && !std::is_const_v<S>, iterator> end() { using std::end; return end(*(this->get())); }
+    std::enable_if_t<has_nonmember_end_v<S> && !std::is_const_v<S>, iterator> end() {
+      using std::end;
+      return end(*(this->get()));
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_begin_v<S>, const_iterator> begin() const { using std::begin; return begin(*(this->get())); }
+    std::enable_if_t<has_nonmember_begin_v<S>, const_iterator> begin() const {
+      using std::begin;
+      return begin(*(this->get()));
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_end_v<S>, const_iterator> end() const { using std::end; return end(*(this->get())); }
+    std::enable_if_t<has_nonmember_end_v<S>, const_iterator> end() const {
+      using std::end;
+      return end(*(this->get()));
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_data_v<S> && !std::is_const_v<S>, pointer> data() { using std::data; return data(*(this->get())); }
+    std::enable_if_t<has_nonmember_data_v<S> && !std::is_const_v<S>, pointer> data() {
+      using std::data;
+      return data(*(this->get()));
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_data_v<S>, const_pointer> data() const { using std::data; return data(*(this->get())); }
+    std::enable_if_t<has_nonmember_data_v<S>, const_pointer> data() const {
+      using std::data;
+      return data(*(this->get()));
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_nonmember_size_v<S>, std::size_t> size() const { using std::size; return size(*(this->get())); }
+    std::enable_if_t<has_nonmember_size_v<S>, std::size_t> size() const {
+      using std::size;
+      return size(*(this->get()));
+    }
 
     template <typename S = Storage>
-    void resize(std::size_t new_size) { return array_adaptor<Storage>::resize(*(this->get()), new_size); }
+    void resize(std::size_t new_size) {
+      if (this->base().index() == 0 && new_size > 0)
+        *this = mohndle(new_size);
+      else
+        array_adaptor<Storage>::resize(*(this->get()), new_size);
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_squarebraket_v<S> && !std::is_const_v<S>, reference> operator[](std::size_t ord) { return (*(this->get()))[ord]; }
+    std::enable_if_t<has_squarebraket_v<S> && !std::is_const_v<S>, reference> operator[](std::size_t ord) {
+      return (*(this->get()))[ord];
+    }
 
     template <typename S = Storage>
-    std::enable_if_t<has_squarebraket_v<S>, const_reference> operator[](std::size_t ord) const { return (*(this->get()))[ord]; }
+    std::enable_if_t<has_squarebraket_v<S>, const_reference> operator[](std::size_t ord) const {
+      return (*(this->get()))[ord];
+    }
 
     template <typename S, typename Enabler>
     friend void swap(mohndle<S>& first, mohndle<S>& second);
@@ -139,7 +184,9 @@ namespace btas {
       return std::visit(
           [](auto&& v) -> const Storage* {
             using v_t = std::remove_reference_t<decltype(v)>;
-            if constexpr (std::is_same_v<v_t, Storage>) {
+            if constexpr (std::is_same_v<v_t, std::monostate> || std::is_same_v<v_t, std::monostate const>) {
+              return &null_storage_;
+            } else if constexpr (std::is_same_v<v_t, Storage>) {
               return &v;
             } else if constexpr (std::is_same_v<v_t, Storage const>) {
               return &v;
@@ -174,23 +221,29 @@ namespace btas {
 
     template <typename Archive>
     void serialize(Archive& ar, const unsigned int = 0) {
-      std::visit(
-          [&ar](auto&& v) -> void {
-            using v_t = std::remove_reference_t<decltype(v)>;
-            if constexpr (std::is_same_v<v_t, Storage> || std::is_same_v<v_t, Storage const> ||
-                          std::is_same_v<v_t, Storage*> || std::is_same_v<v_t, Storage* const> ||
-                          std::is_same_v<v_t, std::unique_ptr<Storage>> ||
-                          std::is_same_v<v_t, std::unique_ptr<Storage> const> ||
-                          std::is_same_v<v_t, std::shared_ptr<Storage>> ||
-                          std::is_same_v<v_t, std::shared_ptr<Storage> const>) {
-              ar& v;
-            } else if constexpr (std::is_same_v<v_t, std::reference_wrapper<Storage>> ||
-                                 std::is_same_v<v_t, std::reference_wrapper<Storage> const>) {
-              abort();
-            } else
-              abort();
-          },
-          this->base());
+      constexpr bool writing = std::is_base_of_v<boost::archive::detail::basic_oarchive, Archive>;
+      constexpr auto serializable_index = std::index_sequence<0, 1, 2, 3, 5>{};
+
+      auto index = this->base().index();
+
+      // abort if trying to store an unsupported case
+      if constexpr (writing) {
+        if (std::holds_alternative<std::reference_wrapper<Storage>>(this->base()))
+          abort();
+      }
+      ar& index;
+      if constexpr (writing)
+        std::visit(
+            [&ar](const auto& v) -> void {
+              using v_t = std::decay_t<decltype(v)>;
+              // - can't read reference_wrapper
+              // - no need to write monostate
+              if constexpr (!std::is_same_v<v_t, std::reference_wrapper<Storage>> && !std::is_same_v<v_t, std::monostate>)
+                ar& v;
+            },
+            this->base());
+      else
+        variant_load_impl(ar, this->base(), index, serializable_index);
     }
 
    private:
@@ -199,6 +252,31 @@ namespace btas {
 
     template <typename Storage_, typename>
     friend void swap(mohndle<Storage_>& first, mohndle<Storage_>& second);
+
+    // utility for serializing select members of variant
+    template <typename Archive, typename... Ts, std::size_t I0, std::size_t... Is>
+    Archive& variant_load_impl(Archive& ar, std::variant<Ts...>& v, std::size_t which, std::index_sequence<I0, Is...>) {
+      constexpr bool writing = std::is_base_of_v<boost::archive::detail::basic_oarchive, Archive>;
+      static_assert(!writing);
+      if (which == I0) {
+        using type = std::variant_alternative_t<I0, std::variant<Ts...>>;
+        if constexpr (!std::is_same_v<type, std::monostate>) {
+          type value;
+          ar& value;
+          v.template emplace<I0>(std::move(value));
+        }
+        else if constexpr (std::is_same_v<type, std::monostate>)
+          v = {};
+      } else {
+        if constexpr (sizeof...(Is) == 0)
+          throw std::logic_error("btas::mohndle::variant_load_impl(ar,v,idx,idxs): idx is not present in idxs");
+        else
+          return variant_load_impl(ar, v, which, std::index_sequence<Is...>{});
+      }
+      return ar;
+    }
+
+    inline static Storage null_storage_ = {};  // used if this is null
   };
 
   template <typename Storage, typename = std::enable_if_t<!std::is_const_v<Storage>>>
@@ -206,6 +284,7 @@ namespace btas {
     using std::swap;
     swap(first.base(), second.base());
   }
+
 }
 
 #endif  // BTAS_UTIL_MOHNDLE_H
