03ef1fc4b921f53b96a2933877502ab482048f36
kmp5VT <kmp5@vt.edu>
2022-05-25 09:13:36 -0400

    Remove typeid function call

--- btas/generic/tuck_cp_als.h
+++ btas/generic/tuck_cp_als.h
@@ -202,7 +202,7 @@ namespace btas{
       detail::get_fit(converge_test, epsilon);
       epsilon = 1 - epsilon;
       // Checks loss function if required
-      if (calculate_epsilon && typeid(converge_test) != typeid(btas::FitCheck<Tensor>)) {
+      if (calculate_epsilon && epsilon == 2) {
         epsilon = this->norm(this->reconstruct() - tensor_ref);
       }
     }
@@ -490,7 +490,7 @@ namespace btas{
       detail::get_fit(converge_test, epsilon);
       epsilon = 1 - epsilon;
       // Checks loss function if required
-      if (calculate_epsilon && typeid(converge_test) != typeid(btas::FitCheck<Tensor>)) {
+      if (calculate_epsilon && epsilon == 2) {
         epsilon = this->norm(this->reconstruct() - tensor_ref);
       }
     }
