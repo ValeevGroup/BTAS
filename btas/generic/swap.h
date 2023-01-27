51ec206459456e30acf81c106a73a2235ab1d693
kshitij-05 <kshitijsurjuse100@gmail.com>
2023-01-27 14:31:55 -0500

    Fixed underscore in wrong place

--- btas/generic/swap.h
+++ btas/generic/swap.h
@@ -21,7 +21,7 @@ namespace btas {
 /// Generalized implementation for mkl_?imatcopy
 template<typename T_>
 void imatcopy(char* ordering, char* trans, size_t rows, size_t cols, T_ alpha, T_ a, size_t src_lda, size_t dst_lda){
-  if constexpr (is_complex_type_v<_T>){
+  if constexpr (is_complex_type_v<T_>){
     mkl_zimatcopy(ordering , trans , rows, cols, alpha , a, src_lda, dst_lda);
   }
   else{
