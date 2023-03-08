f9136ead44d0c90aae170c669566a5f04c7c5051
Eduard Valeyev <eduard@valeyev.net>
2023-03-08 09:11:41 -0500

    moar spaces

--- btas/generic/cp_als.h
+++ btas/generic/cp_als.h
@@ -686,7 +686,7 @@ namespace btas {
       }
       KRP_dims.push_back(ndim);
 
-      contract(this-> one , tensor_ref, tref_indices, KhatriRao, KRP_dims, this-> zero, temp, An_indices);
+      contract(this->one, tensor_ref, tref_indices, KhatriRao, KRP_dims, this->zero, temp, An_indices);
 
       // move the nth mode of the reference tensor back where it belongs
       swap_to_first(tensor_ref, n, true);
@@ -694,8 +694,7 @@ namespace btas {
 #else  // BTAS_HAS_CBLAS
       // without MKL program cannot perform the swapping algorithm, must compute
       // flattened intermediate
-      gemm(blas::Op::NoTrans, blas::Op::NoTrans, this->one, flatten(tensor_ref, n), this->generate_KRP(n, rank, true),
-           this->zero, temp);
+      gemm(blas::Op::NoTrans, blas::Op::NoTrans, this->one, flatten(tensor_ref, n), this->generate_KRP(n, rank, true), this->zero, temp);
 #endif
 
       if(lambda != 0){
@@ -765,8 +764,8 @@ namespace btas {
                 Range1{last_dim ? size / tensor_ref.extent(contract_dim) : tensor_ref.extent(contract_dim)}});
 
       // contract tensor ref and the first factor matrix
-      gemm((last_dim ? blas::Op::Trans : blas::Op::NoTrans), blas::Op::NoTrans, this->one,
-           (last_dim ? tensor_ref.conj() : tensor_ref), A[contract_dim].conj(), this->zero, temp);
+      gemm((last_dim ? blas::Op::Trans : blas::Op::NoTrans), blas::Op::NoTrans, this->one , (last_dim? tensor_ref.conj():tensor_ref), A[contract_dim].conj(), this->zero,
+           temp);
 
       // Resize tensor_ref
       tensor_ref.resize(R);
