d6fd0e6e5f15f71b10478c18b5c2e037dc2e4903
Karl Pierce <kmp5@vt.edu>
2024-01-22 15:55:42 -0500

    remove extraneous curly brace

--- btas/generic/cp_als.h
+++ btas/generic/cp_als.h
@@ -658,8 +658,7 @@ namespace btas {
                       double lambda = 0) {
       Tensor temp(A[n].extent(0), rank);
       Tensor an(A[n].range());
-
-          }
+      
 #ifdef BTAS_HAS_INTEL_MKL
 
       // Computes the Khatri-Rao product intermediate
