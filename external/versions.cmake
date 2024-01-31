45ca46da2f1dd8266aaddcfed72e2e7b35a75a85
Eduard Valeyev <eduard@valeyev.net>
2024-01-31 17:00:32 -0500

    [cmake] insist on Boost 1.71 or later if discovering

--- external/versions.cmake
+++ external/versions.cmake
@@ -1,7 +1,7 @@
 set(BTAS_TRACKED_VGCMAKEKIT_TAG d5c0a6f9ff6dc97cbb5132912733e1eb1cf73f1e)
 
-# likely can use earlier, but
+# oldest Boost we can tolerate ... likely can use an earlier version, but:
 # - as of oct 2023 tested with 1.71 and up only
 # - avoids the need to avoid 1.70 in which Boost.Container is broken
 # - matches the version provided by https://github.com/Orphis/boost-cmake as of oct 2023
-set(BTAS_TRACKED_BOOST_VERSION 1.71)
+set(BTAS_OLDEST_BOOST_VERSION 1.71)
